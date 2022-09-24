72fe6c26dfcb83928ab15b6ab50477250c747875
Ko2doo <ko2doodev@gmail>
2022-09-24 23:37:00 +0500

    UPD => небольшие фиксы в именах функци и описании

--- json_convertor.py
+++ json_convertor.py
@@ -16,7 +16,7 @@ def json_converter(data):
 
 
 # Читаем файл
-def reading_file_and_create_tuple():
+def reading_file_and_create_array():
     data = list()
 
     while True:
@@ -34,11 +34,11 @@ def reading_file_and_create_tuple():
                     print(":------***------:")
 
                 # октроем файл для чтения если такой существует
-                source_filepath = open(source_path, "rt", encoding="utf-8")
+                reading_file = open(source_path, "rt", encoding="utf-8")
                 # если файл существует, запишем строки из файла, в кортеж
                 bar = IncrementalBar('Writing...', max = len(data))
 
-                for string in source_filepath:
+                for string in reading_file:
                     bar.next()
                     string = string.rstrip().replace("\t", " ")  # убираем последний символ '\n' из string
                     data.append(string)
@@ -47,22 +47,20 @@ def reading_file_and_create_tuple():
                 # проверяем содержимое кортежа и выводим информацию в консоль
                 if len(data) > 0:
                     source_name = source_path
-                    print("Содержимое файла", source_name, "успешно записаны в кортеж.\n", "Количество строк в кортеже  - ", len(data))
+                    print("Содержимое файла", source_name, "успешно записаны в массив.\n", "Количество строк в массиве  - ", len(data))
                     print(":------***------:")
-                    custom_name = json_converter(data)  # Вызывваем функцию нейминга файла и генератора json
+                    starting_converter = json_converter(data)  # Вызывваем функцию нейминга файла и генератора json
                     print(":------***------:")
                 else:
                     print("None")
                 sys.exit()
             else:
                 print("Неправильный путь, повторите попытку снова")
-
-
         except OSError as e:
             print(e.errno)
 
 
-def input_state():
+def main():
     # бесконечный цикл, который продолжает выполняться
     # до возникновения исключения
     while True:
@@ -70,17 +68,15 @@ def input_state():
             contin_mod = 1
             exit_mod = 2
 
-            hello_world = "Чтобы продолжить введите 1, чтобы выйти введите 2: "
+            program_option_txt = "Добро пожаловать в мини-программу по конвертации данных из .txt в .json\nЧтобы продолжить введите 1, чтобы выйти введите 2: "
 
-            start_prog = int(input(hello_world))  # int() Задаём тип вводимых знаков, а именно числовые
-            user_value = start_prog
+            program_option = int(input(program_option_txt))  # int() Задаём тип вводимых знаков, а именно числовые
+            user_value = program_option
 
             if user_value == contin_mod:
-                file_path = reading_file_and_create_tuple()
+                file_path = reading_file_and_create_array()
             elif user_value == exit_mod:
                 sys.exit()  # выход из программы
-
-
         except ValueError:
             # цикл будет повторяться до правильного ввода
             error_txt = "Ошибка! Это не число, попробуйте снова."
@@ -88,7 +84,4 @@ def input_state():
         else:
             break
 
-input_state()
-
-
-
+main()
